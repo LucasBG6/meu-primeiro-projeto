9a167c16bae5a9a76493c29f39579ea71f985e94
Lucas <beluci441@gmail.com>
2024-09-20 20:37:53 -0300

    CarStore

---
+++ carstore/src/main/java/com/aula/carstore/servlet/CreateCarServlet.java
@@ -0,0 +1,9 @@
+package com.aula.carstore.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/create-car")
+public class CreateCarServlet extends HttpServlet {
+
+}
